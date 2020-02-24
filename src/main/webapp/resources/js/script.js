a2b5dd03deb43d57076670e11bd161010576f689
konecmi4 <konecmi4@fit.cvut.cz>
2020-02-24 21:47:51 +0100

    Pred nahranim zobrazeni obrazku + styl formulare

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,6 +1,6 @@
 (function($) {
 
-    $('#file').change(function() {
+    /*$('#file').change(function() {
         var i = $(this).prev('label').clone();
         var file = $('#file')[0].files[0].name;
         //$(this).prev('label').text(file);
@@ -12,41 +12,27 @@
             $('.label--file').after('<span class="file-name">' + file + '</span>');
         }
         console.log(file);
-    });
+    });*/
 
-})( jQuery );
-
-/*
-function readURL(input) {
+    function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
-            
+            var name = $('#file')[0].files[0].name;
             reader.onload = function (e) {
-            $('.label--file').append('<img src="' + e.target.result + '" class="file-name" width="200"/>');
+                $('.list--gallery').append(
+                    '<li class="list--item">' +
+                    '<img src="' + e.target.result + '" class="file-name" alt="'+ name +'" width="200"/>' +
+                    '<a href="' + e.target.result +'">Zvětšit</a>' +
+                    '</li>');
                 //$('#blah').attr('src', e.target.result);
-            }
-            
+            };
+
             reader.readAsDataURL(input.files[0]);
         }
     }
-    
-    $("#imgInp").change(function(){
+
+    $("#file").change(function(){
         readURL(this);
     });
-    
-$('#imgInp').change(function() {
-        var i = $(this).prev('label').clone();
-        var file = $('#imgInp')[0].files[0].name;
-        //$(this).prev('label').text(file);
 
-				$('.label--file').append('<span class="file-name">' + file + '</span>');
-
-        /*var file_name = $('.file-name');
-        if(file_name.length){
-            file_name.text(file);
-        }else{
-            
-        }*/
-        //console.log(file);
-    });
-*/
+})( jQuery );
