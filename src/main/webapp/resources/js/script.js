1be2a4b01a490ee2d1d844d1f74676c63a4d0540
Michal Konečný <konecmi4@fit.cvut.cz>
2020-02-24 13:51:10 +0100

    Vyzkouset doma

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -15,3 +15,38 @@
     });
 
 })( jQuery );
+
+/*
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+            $('.label--file').append('<img src="' + e.target.result + '" class="file-name" width="200"/>');
+                //$('#blah').attr('src', e.target.result);
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+    $("#imgInp").change(function(){
+        readURL(this);
+    });
+    
+$('#imgInp').change(function() {
+        var i = $(this).prev('label').clone();
+        var file = $('#imgInp')[0].files[0].name;
+        //$(this).prev('label').text(file);
+
+				$('.label--file').append('<span class="file-name">' + file + '</span>');
+
+        /*var file_name = $('.file-name');
+        if(file_name.length){
+            file_name.text(file);
+        }else{
+            
+        }*/
+        //console.log(file);
+    });
+*/
